172377a735b5b106fff9783fcbd37366fa9b43a0
samresh18 <dhawan.samresh@gmail.com>
2019-04-23 00:43:38 +0530

    Changes done

--- SonarQubeJenkinsTest/Controllers/HomeController.cs
+++ SonarQubeJenkinsTest/Controllers/HomeController.cs
@@ -12,6 +12,11 @@ namespace SonarQubeJenkinsTest.Controllers
         {
             string i = "Test";
             string UU = "";
+            int target = -5;
+            int num = 3;
+
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
             return View();
         }
 
